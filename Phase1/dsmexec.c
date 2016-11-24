5524aa7dc555b97b9266997c4c9404ca053c3cab
MALEUVRE Alexis <amaleuvre@waimea.pedago.ipb.fr>
2016-11-24 10:06:14 +0100

    j'adore commit

--- Phase1/dsmexec.c
+++ Phase1/dsmexec.c
@@ -24,8 +24,8 @@ void usage(void)
 
 void sigchld_handler(int sig)
 {
-   /* on traite les fils qui se terminent */
-   /* pour eviter les zombies */
+  /* on traite les fils qui se terminent */
+  /* pour eviter les zombies */
 }
 
 
@@ -34,85 +34,90 @@ int main(int argc, char *argv[])
   if (argc < 3){
     usage();
   } else {
-     pid_t pid;
-     int num_procs = 0;
-     int i;
+    pid_t pid;
+    int num_procs = 0;
+    int i;
 
-     /* Mise en place d'un traitant pour recuperer les fils zombies*/
-     /* XXX.sa_handler = sigchld_handler; */
+    /* Mise en place d'un traitant pour recuperer les fils zombies*/
+    /* XXX.sa_handler = sigchld_handler; */
 
-     /* lecture du fichier de machines */
-     /* 1- on recupere le nombre de processus a lancer */
-     /* 2- on recupere les noms des machines : le nom de */
-     /* la machine est un des elements d'identification */
+    /* lecture du fichier de machines */
+    /* 1- on recupere le nombre de processus a lancer */
+    /* 2- on recupere les noms des machines : le nom de */
+    /* la machine est un des elements d'identification */
 
-     /* creation de la socket d'ecoute */
-     /* + ecoute effective */
+    /* creation de la socket d'ecoute */
+    /* + ecoute effective */
 
-     /* creation des fils */
-     for(i = 0; i < num_procs ; i++) {
+    /* creation des fils */
+    for(i = 0; i < num_procs ; i++) {
 
-	/* creation du tube pour rediriger stdout */
+      /* creation du tube pour rediriger stdout */
+      int pip_out[2];
+      pipe(pip_out);
 
-	/* creation du tube pour rediriger stderr */
-
-	pid = fork();
-	if(pid == -1) ERROR_EXIT("fork");
-
-	if (pid == 0) { /* fils */
-
-	   /* redirection stdout */
-
-	   /* redirection stderr */
-
-	   /* Creation du tableau d'arguments pour le ssh */
-
-	   /* jump to new prog : */
-	   /* execvp("ssh",newargv); */
-
-	} else  if(pid > 0) { /* pere */
-	   /* fermeture des extremites des tubes non utiles */
-	   num_procs_creat++;
-	}
-     }
+      /* creation du tube pour rediriger stderr */
+      int pip_err[2];
+      pipe(pip_err);
 
 
-     for(i = 0; i < num_procs ; i++){
+      pid = fork();
+      if(pid == -1) ERROR_EXIT("fork");
 
-	/* on accepte les connexions des processus dsm */
+      if (pid == 0) { /* fils */
 
-	/*  On recupere le nom de la machine distante */
-	/* 1- d'abord la taille de la chaine */
-	/* 2- puis la chaine elle-meme */
+        /* redirection stdout */
 
-	/* On recupere le pid du processus distant  */
+        /* redirection stderr */
 
-	/* On recupere le numero de port de la socket */
-	/* d'ecoute des processus distants */
-     }
+        /* Creation du tableau d'arguments pour le ssh */
 
-     /* envoi du nombre de processus aux processus dsm*/
+        /* jump to new prog : */
+        /* execvp("ssh",newargv); */
 
-     /* envoi des rangs aux processus dsm */
+      } else  if(pid > 0) { /* pere */
+        /* fermeture des extremites des tubes non utiles */
+        num_procs_creat++;
+      }
+    }
 
-     /* envoi des infos de connexion aux processus */
 
-     /* gestion des E/S : on recupere les caracteres */
-     /* sur les tubes de redirection de stdout/stderr */
-     /* while(1)
-         {
-            je recupere les infos sur les tubes de redirection
-            jusqu'à ce qu'ils soient inactifs (ie fermes par les
-            processus dsm ecrivains de l'autre cote ...)
+    for(i = 0; i < num_procs ; i++){
 
-         };
-      */
+      /* on accepte les connexions des processus dsm */
 
-     /* on attend les processus fils */
+      /*  On recupere le nom de la machine distante */
+      /* 1- d'abord la taille de la chaine */
+      /* 2- puis la chaine elle-meme */
 
-     /* on ferme les descripteurs proprement */
+      /* On recupere le pid du processus distant  */
 
-     /* on ferme la socket d'ecoute */
-  }
-   exit(EXIT_SUCCESS);
+      /* On recupere le numero de port de la socket */
+      /* d'ecoute des processus distants */
+    }
+
+    /* envoi du nombre de processus aux processus dsm*/
+
+    /* envoi des rangs aux processus dsm */
+
+    /* envoi des infos de connexion aux processus */
+
+    /* gestion des E/S : on recupere les caracteres */
+    /* sur les tubes de redirection de stdout/stderr */
+    /* while(1)
+    {
+    je recupere les infos sur les tubes de redirection
+    jusqu'à ce qu'ils soient inactifs (ie fermes par les
+    processus dsm ecrivains de l'autre cote ...)
+
+  };
+  */
+
+  /* on attend les processus fils */
+
+  /* on ferme les descripteurs proprement */
+
+  /* on ferme la socket d'ecoute */
+}
+exit(EXIT_SUCCESS);
 }
